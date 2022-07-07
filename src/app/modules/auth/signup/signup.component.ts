9efa1cf8e1b703961a4916286059f011a4478abc
nguyenhaininh <hulkhulk1245@gmai.com>
2022-07-07 09:01:07 +0700

    update

--- src/app/modules/auth/signup/signup.component.ts
+++ src/app/modules/auth/signup/signup.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ok } from 'assert';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../../../@core/services/auth.service';
+
 
 @Component({
   selector: 'ngx-signup',
@@ -13,33 +14,52 @@ import { AuthService } from '../../../@core/services/auth.service';
 export class SignupComponent implements OnInit {
   formSignup: FormGroup;
   isSubmitted = false;
-
+  isSaving = false;
   constructor(private authService: AuthService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
     this.initForm();
+
   }
 
   initForm() {
     this.formSignup = this.fb.group({
-      userName: ['', Validators.required],
-      password: ['', Validators.required],
-      email: ['', Validators.required],
-      newPassword: ['', Validators.required],
+      userName: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$')]],
+      email: ['', [Validators.required, Validators.email]],
+      newPassword: ['', [Validators.required]],
       birthDay: ['', Validators.required],
-      phoneNumber: ['', Validators.required],
-      fullName: "ninh",
-      name: "ninh"
+      phoneNumber: ['', [Validators.required, Validators.pattern('^[0-9]')]],
+      fullName: "xuan",
+      name: "pahm"
+      // userName: "xuan",
+      // password: "123456",
+      // email: "hoang",
+      // newPassword: "123456",
+      // birthDay: "2020-07-12",
+      // phoneNumber: "12354543",
+      // fullName: "xuan",
+      // name: "pahm"
     });
   }
 
+  get f() {
+    return this.formSignup.controls;
+  }
+
   signup() {
+
     this.isSubmitted = true;
     if (this.formSignup.valid) {
-      this.authService.signup(this.formSignup.value).subscribe();
-
-      alert("Signup successful!");
-      this.router.navigate(['/auth']);
+      this.authService.signup(this.formSignup.value).subscribe(
+        response => {
+          alert("Signup successful!");
+          this.router.navigate(['/auth']);
+        },
+        error => {
+          // You can access status:
+          console.log(error.status);
+        });
     }
   }
 
